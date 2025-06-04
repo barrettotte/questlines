0b34f666cbb5cff0ec94a2a999e94f130e051d27
barrettotte <barrettotte@gmail.com>
2025-06-04 00:32:53 -0400

    adds github action for browser-only build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,32 +1,40 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const base = (mode === 'browseronly' && env.GITHUB_DEPLOY === 'true')
+             ? `${process.env.GITHUB_REPO_NAME || 'questlines'}`
+             : '/';
+
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    base: base,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8080',
+          changeOrigin: true,
+        }
       }
+    },
+    build: {
+      outDir: 'dist',
+      sourcemap: true,
+      minify: mode == 'production' || mode == 'browseronly',
     }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    minify: mode == 'production',
   }
-}));
+});
